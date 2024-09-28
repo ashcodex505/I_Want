6593403cce8f218b40a6ca724615590364a96b46
Abhave Abhilash <abhaveabhilash@gmail.com>
2024-09-28 10:58:28 -0700

    made docker file and working on adding test data

---
+++ back-end/app.py
@@ -0,0 +1,13 @@
+# make a default boilerplate for flask
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
